2332d80a0f59ac47be60b144c6489246899c6156
gmrosas <56293154+gmrosas@users.noreply.github.com>
2026-06-07 20:52:00 +0200

    Update streamlit_app.py
    Add https://my.smoothiefroot.com/api/fruit/watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+    # New section to display smoothiefruit nutrition information
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
+  
     st.write(ingredients_string)
 
     my_insert_stmt = """insert into smoothies.public.orders (ingredients, name_on_order)
